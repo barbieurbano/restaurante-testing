e477a4a63ada5e7d84702706d14f972751a8ad51
BarbaraUrbano <sbaarby@gmail.com>
2026-04-19 20:30:42 +0200

    Initial commit

---
+++ src/main/java/com/restaurante/restaurantetesting/RestauranteTestingApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.restaurantetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestauranteTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/restaurante/restaurantetesting/RestauranteTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.restaurantetesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

419a42907caa68785da63979d292581a99a5533c
Barbara Urbano <sbaarby@gmail.com>
2026-06-01 03:08:19 +0200

    Se agregan dependencias en pom, para las SUITE de tests

--- src/test/java/com/restaurante/restaurantetesting/ControllerTestsSuite.java
+++ src/test/java/com/restaurante/restaurantetesting/ControllerTestsSuite.java
@@ -1,4 +1,20 @@
 package com.restaurante.restaurantetesting;
 
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+/*
+
+Suite para ejecutar los tests de repositorios
+
+por terminal:
+
+./mvnw test
+./mvnw test -Dtest=RepositoryTestsSuite
+ */
+
+
+@Suite
+@SelectPackages({"com.restaurante.restaurantetesting.controller"})
 public class ControllerTestsSuite {
 }
--- src/test/java/com/restaurante/restaurantetesting/RepositoryTestsSuite.java
+++ src/test/java/com/restaurante/restaurantetesting/RepositoryTestsSuite.java
@@ -1,4 +1,9 @@
 package com.restaurante.restaurantetesting;
 
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages({"com.restaurante.restaurantetesting.repository"})
 public class RepositoryTestsSuite {
 }
